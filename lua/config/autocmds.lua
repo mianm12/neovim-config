ef07edf5ebeee8801a27df28f3dc84a577fa5093
mianm <98447854+mianm12@users.noreply.github.com>
2023-12-13 18:38:56 +0800

    update

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- 在Makefile中使用tabs
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "make" },
+  callback = function()
+    vim.opt.expandtab = false -- 使用跳格(tabs)代替空格(spaces)
+  end,
+})
